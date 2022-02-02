8b1a46b1e8e439ac8e2cf112bc7dcc43ab03e26b
Ramón Martínez <ra1996@gmail.com>
2022-02-02 23:45:20 +0100

    unified install

--- install.sh
+++ install.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+		echo
+		echo "This script must be run as root." 1>&2
+		echo
+		exit 1
+fi
+
 if [-z "$GOROOT"]; then
     echo "GOROOT env variable not set. Please set it before executing this script."
 fi
 
-mkdir $GOROOT/scan
-mkdir $GOROOT/ports
+if [!-d "$GOROOT/src/arping"]; then
+    git clone https://github.com/j-keck/arping -o $GOROOT/src/arping
+fi
 
-cp scan.go $GOROOT/scan
-cp ports.go $GOROOT/ports
+if [!-d "$GOROOT/src/scan"];then
+    mkdir $GOROOT/src/scan
+fi
+
+if [!-d "$GOROOT/src/ports"]; then
+    mkdir $GOROOT/src/ports
+fi
+
+cp scan.go $GOROOT/src/scan
+cp ports.go $GOROOT/src/ports
 
 go build scannerPort.go

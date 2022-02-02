b2141bf8bd3312deb431e2188bf50d25effd3273
Ramón Martínez <ra1996@gmail.com>
2022-02-02 22:08:04 +0100

    added more ports

--- ports.go
+++ ports.go
@@ -1,41 +1,48 @@
 package ports
-func Ports() map[int]string {
-    //Based in well known ports
-    ports := map[int]string {
-        1:"echo",
-        9:"WOL",
-        20:"ftp data",
-        21:"ftp control",
-        22:"ssh",
-        23:"telnet",
-        25:"smtp",
-        43:"whois",
-        49:"TACACS",
-        53:"DNS",
-        67:"BOOTP",
-        69:"TFTP",
-        70:"Gopher",
-        71:"NETRJS",
-        80:"http",
-        81:"TorPark",
-        82:"TorPark",
-        88:"Kerberos",
-        123:"NTP",
-        135:"RPC",
-        443:"https",
-        445:"Microsoft-ds, Samba",
-        465:"SMTP over TLS",
-        514:"Syslog",
-        520:"RIP",
-        521:"RIPng",
-        540:"UUCP",
-        543:"klogin",
-        544:"kshell",
-        
 
-        3389:"rdp"
-        6667:"irc"
-        25565:"minecraft"
-    }
-    return ports
+func Ports() map[int]string {
+	//Based in well known ports
+	ports := map[int]string{
+		1:     "echo",
+		9:     "WOL",
+		20:    "ftp data",
+		21:    "ftp control",
+		22:    "ssh",
+		23:    "telnet",
+		25:    "smtp",
+		43:    "whois",
+		49:    "TACACS",
+		53:    "DNS",
+		67:    "BOOTP",
+		69:    "TFTP",
+		70:    "Gopher",
+		71:    "NETRJS",
+		80:    "http",
+		81:    "TorPark",
+		82:    "TorPark",
+		88:    "Kerberos",
+		110:   "POP3",
+		115:   "sFTP",
+		220:   "imap3",
+		123:   "NTP",
+		135:   "RPC",
+		443:   "https",
+		445:   "Microsoft-ds, Samba",
+		465:   "SMTP over TLS",
+		514:   "Syslog",
+		520:   "RIP",
+		521:   "RIPng",
+		540:   "UUCP",
+		543:   "klogin",
+		544:   "kshell",
+		993:   "IMAP over TLS",
+		995:   "POP3 over TLS",
+		1433:  "Microsoft SQL Server",
+		3306:  "MySQL",
+		3389:  "rdp",
+		5432:  "postgres",
+		6667:  "irc",
+		25565: "minecraft",
+	}
+	return ports
 }
